8efab20198f4bcea5419001e73faa649b39110c3
spark <sparkcarranza@gmail.com>
2013-04-07 15:46:32 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mblog::Application.config.secret_token = 'b984e3504540f167178b21dbe469c8cd9564e86b49ce2c0554720fe89a978d791eebcef376a9be2429b5199f1db01e72e4c4f610a2018d9059593e8c41685201'
