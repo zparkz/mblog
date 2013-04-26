5bc056a0e15456e44730a00e1babe827e40145aa
Kris <andagi@gmail.com>
2013-04-25 18:34:56 -0700

    Added Alison's template

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Mblog::Application.routes.draw do
   authenticated :user do
     root :to => 'posts#index'
   end
-  root :to => "posts#index"
+  root :to => "home#index"
   #devise_for :users
   resources :users 
 
