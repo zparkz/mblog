630c681341ead79067f98fa5101b5913321aa2f5
arossUK <aross@mills.edu>
2013-04-26 21:31:56 -0700

    RSPEC AND CUCUMBER CHANGES FROM ALISON - FORGOT TO COMMIT BEFORE MERGING TO MASTER DAMMIT!

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe HomeController do
+	describe "#home" do
+		it "renders the home template" do
+      		get :home
+      		expect(response).to render_template("home")
+      		response.should be_success
+    	end
+end
 end
