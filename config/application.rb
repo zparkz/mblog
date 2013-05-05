5e77506418148cdf53f4976397b0481ab5c7d2e2
spark <sparkcarranza@gmail.com>
2013-05-05 01:48:15 -0700

    modifying buttons on pages, adding icons

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,7 @@ module Mblog
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.prefix = '/assets'
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
